69e9fb2d82fbfc65f3ac1560a69f2974324a0d05
micalgenus <micalgenus@gmail.com>
2020-06-13 17:13:19 +0900

    Add health check in kubernetes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+// For kubernetes
+app.get('/healthz', (req, res) => {
+  res.send('OK');
+});
+
+// For test
 app.post('/ping', (req, res) => {
   res.send('Pong !!');
 });
