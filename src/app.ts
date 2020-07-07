3291afed53151dd5f814d08f044557d0c15f945c
micalgenus <micalgenus@gmail.com>
2020-07-07 23:16:00 +0900

    Add file upload handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import p from 'path';
+import multer from 'multer';
 
 import { getUniqueRandomHash } from '@/core/utils';
 import redis from '@/core/redis';
 
 const app = express();
+const upload = multer({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,7 +29,22 @@ app.post('/upload/text', async (req, res, next) => {
 
   try {
     const { namespace, password } = await getUniqueRandomHash();
-    redis.hmset(namespace, { [p.resolve('/' + path)]: text, password });
+    redis.hmset(namespace, { [p.resolve('/' + path)]: text, password, type: 'text' });
+
+    res.json({ namespace, password });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/upload/file', upload.single('file'), async (req, res, next) => {
+  if (!req.file) {
+    res.status(500).send('Invalid file');
+  }
+
+  try {
+    const { namespace, password } = await getUniqueRandomHash();
+    await redis.hmset(namespace, { [p.resolve('/' + req.file.originalname)]: req.file.buffer.toString('base64'), password, type: 'file' });
 
     res.json({ namespace, password });
   } catch (err) {
@@ -37,22 +55,36 @@ app.post('/upload/text', async (req, res, next) => {
 app.get('/download/:namespace/:path(*)', async (req, res, next) => {
   try {
     const { namespace, path } = req.params;
+    console.log(namespace, path);
     const findPath = p.resolve('/' + path);
 
-    const texts = await redis.hmget(namespace, findPath);
+    const texts = await redis.hmget(namespace, findPath, 'type');
+    const { type } = texts;
+
+    console.log(texts);
 
     if (!texts[findPath]) {
       res.status(404);
     }
 
-    res.send(texts[findPath]);
+    let text = texts[findPath];
+    switch (type) {
+      case 'text':
+        break;
+      case 'file':
+        text = new Buffer(text, 'base64').toString('ascii');
+        break;
+      default:
+        throw new Error('Invalid item');
+    }
+
+    res.send(text);
   } catch (err) {
     next(err);
   }
 });
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack, err, res, next);
   res.status(500).send(process.env.NODE_ENV === 'test' ? JSON.stringify(err.stack) : '');
 });
 
